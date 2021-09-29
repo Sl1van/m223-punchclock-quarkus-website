5e71dd9c7134db4ac80aacc04dfadf588052b890
Sl1van <58076574+Sl1van@users.noreply.github.com>
2021-09-29 15:40:30 +0200

    created AuthService

---
+++ src/app/auth/AuthService.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public constructor(
+    private http: HttpClient,
+  ) { }
+
+  public login(data : any): Observable<any>{
+    // submitForm form data on server and get a peax redirect object with the urls to redirect to
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/text'
+    });
+
+    data = JSON.stringify(data);
+
+    // set form data params
+    return  this.http.post(
+      'http://'+environment.restUrl + '/auth/login',
+      data,
+      { headers}
+    )
+  }
+}
+
